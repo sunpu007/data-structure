74f98e22d2385640fc45c65387548a6a07e208b6
Jerry <731238433@qq.com>
2019-12-05 23:38:36 +0800

    完善动态数组

--- 01-动态数组/src/cn/myjerry/ArrayList.java
+++ 01-动态数组/src/cn/myjerry/ArrayList.java
@@ -1,121 +1,129 @@
 package cn.myjerry;
 
+@SuppressWarnings("all")
 public class ArrayList<E> {
+	
+	/**
+	 * 元素的数量
+	 */
+	private int size;
+	/**
+	 * 所有的元素
+	 */
+	private E[] elements;
+	
+	private static final int DEFAULT_CAPACITY = 10;
+	private static final int ELEMENT_NOT_FOUND = -1;
+	
+	public ArrayList(int capaticy) {
+		capaticy = (capaticy < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capaticy;
+		elements = (E[]) new Object[capaticy];
+	}
 
-    private int size;
+	public ArrayList() {
+		this(DEFAULT_CAPACITY);
+	}
 
-    private E[] elements;
+	/**
+	 * 清除所有元素
+	 */
+	public void clear() {
+		size = 0;
+	}
 
-    private static final int DEFAULT_CAPACITY = 10;
-    private static final int ELEMENT_NOT_FOUND = -1;
+	/**
+	 * 元素的数量
+	 * @return
+	 */
+	public int size() {
+		return size;
+	}
 
-    public ArrayList(int capaticy) {
-        capaticy = (capaticy < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capaticy;
-        elements = (E[]) new Object[capaticy];
-    }
+	/**
+	 * 是否为空
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    public ArrayList() {
-        this(DEFAULT_CAPACITY);
-    }
+	/**
+	 * 是否包含某个元素
+	 * @param element
+	 * @return
+	 */
+	public boolean contains(E element) {
+		return indexOf(element) != ELEMENT_NOT_FOUND;
+	}
 
-    /**
-     * 清除所有元素
-     */
-    public void clear() {
-        size = 0;
-    }
+	/**
+	 * 添加元素到尾部
+	 * @param element
+	 */
+	public void add(E element) {
+		elements[size++] = element;
+	}
 
-    /**
-     * 元素的数量
-     * @return
-     */
-    public int size() {
-        return size;
-    }
+	/**
+	 * 获取index位置的元素
+	 * @param index
+	 * @return
+	 */
+	public E get(int index) {
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("index:"+index+",size"+size);
+		}
+		return elements[index];
+	}
 
-    /**
-     * 是否为空
-     * @return
-     */
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	/**
+	 * 设置index位置的元素
+	 * @param index
+	 * @param element
+	 * @return 原来的元素ֵ
+	 */
+	public E set(int index, E element) {
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("index:"+index+",size"+size);
+		}
+		E old = elements[index];
+		elements[index] = element;
+		return old;
+	}
 
-    /**
-     * 是否包含某个元素
-     * @param element
-     * @return
-     */
-    public boolean contains(E element) {
-        return indexOf(element) != ELEMENT_NOT_FOUND;
-    }
+	/**
+	 * 在index位置插入一个元素
+	 * @param index
+	 * @param element
+	 */
+	public void add(int index, E element) {
+	}
 
-    /**
-     * 添加元素到尾部
-     * @param element
-     */
-    public void add(E element) {
-    }
+	/**
+	 * 删除index位置的元素
+	 * @param index
+	 * @return
+	 */
+	public E remove(int index) {
+		return null;
+	}
 
-    /**
-     * 获取index位置的元素
-     * @param index
-     * @return
-     */
-    public E get(int index) {
-        return elements[index];
-    }
-
-    /**
-     * 设置index位置的元素
-     * @param index
-     * @param element
-     * @return 原来的元素ֵ
-     */
-    public E set(int index, E element) {
-        E old = elements[index];
-        elements[index] = element;
-        return old;
-    }
-
-    /**
-     * 在index位置插入一个元素
-     * @param index
-     * @param element
-     */
-    public void add(int index, E element) {
-    }
-
-    /**
-     * 删除index位置的元素
-     * @param index
-     * @return
-     */
-    public E remove(int index) {
-        return null;
-    }
-
-    /**
-     * 查看元素的索引
-     * @param element
-     * @return
-     */
-    public int indexOf(E element) {
-        if (element == null) {
-            for(int i = 0; i < size; i++) {
-                if (elements[i] == null) {
-                    return i;
-                }
-            }
-        } else {
-            for(int i = 0; i < size; i++) {
-                if (element.equals(elements[i])) {
-                    return i;
-                }
-            }
-        }
-
-        return ELEMENT_NOT_FOUND;
-    }
+	/**
+	 * 查看元素的索引
+	 * @param element
+	 * @return
+	 */
+	public int indexOf(E element) {
+		if (element == null) {
+			for(int index = 0; index < size; index++) {
+				if (elements[index] == null) return index;
+			}
+		} else {
+			for(int index = 0; index < size; index++) {
+				if (elements[index] == element) return index;
+			}
+		}
+		return ELEMENT_NOT_FOUND;
+	}
 
 }
--- 01-动态数组/src/cn/myjerry/Main.java
+++ 01-动态数组/src/cn/myjerry/Main.java
@@ -1,9 +1,17 @@
 package cn.myjerry;
 
 
-public class Main {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        System.out.println(list.isEmpty());
-    }
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(list.size());
+		System.out.println(list.indexOf(2));
+		System.out.println(list.contains(1));
+	}
+
 }
